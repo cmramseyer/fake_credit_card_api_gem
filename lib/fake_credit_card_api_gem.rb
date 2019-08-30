476bf47ff764a2ab09ef1d95d20066ca274948cc
Carlos Ramseyer <carlos@bytelion.com>
2019-08-30 01:30:47 -0300

    refactorized header

--- lib/fake_credit_card_api_gem.rb
+++ lib/fake_credit_card_api_gem.rb
@@ -22,16 +22,13 @@ module FakeCreditCardApiGem
 
   CHECK_PATH = '/check_credit_card'
   CREDIT_CARDS_PATH = '/credit_cards'
+  HEADER = 
 
   def self.checker(number:, code:)
     api_path = CHECK_PATH
     url = "http://#{configuration.host}:#{configuration.port}#{api_path}"
     query = { number: number, code: code }
-    headers = {
-        'Authorization'=>"Bearer #{configuration.api_key}",
-        'Content-Type' =>'application/json',
-        'Accept'=>'application/json'
-    }
+    headers = header(configuration.api_key)
     begin
       response = HTTParty.get(url, query: query, headers: headers)
       body = JSON.parse(response.body)
@@ -44,16 +41,20 @@ module FakeCreditCardApiGem
     api_path = CREDIT_CARDS_PATH
     url = "http://#{configuration.host}:#{configuration.port}#{api_path}"
     query = {}
-    headers = {
-        'Authorization'=>"Bearer #{configuration.api_key}",
-        'Content-Type' =>'application/json',
-        'Accept'=>'application/json'
-    }
+    headers = header(configuration.api_key)
     begin
       response = HTTParty.get(url, query: query, headers: headers)
       body = JSON.parse(response.body)
     rescue Errno::ECONNREFUSED
       body = { message: 'error connecting, API server seems down' }
     end
+  end
+
+  def self.header(api_key)
+    {
+      'Authorization'=>"Bearer #{api_key}",
+      'Content-Type' =>'application/json',
+      'Accept'=>'application/json'
+    }
   end
 end
